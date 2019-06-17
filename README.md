27bbb344c3613a2641dd4326eb82a0536548910d
xiaobai <419456942@qq.com>
2019-06-17 11:51:52 +0800

    webpack

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Webpack4.0
 #第一步初始化一个package.json的文件
 ### npm init -y
+### npm i -webpack
+###如果版本超过4.0的需要额外装-(npm i -D webpack-cli)
+#第二步 安装（npm i lodsh -P）
+#第三步 安装（npm i -D style-loader css-loader）
+#第四步 安装（npm i -D sass-loader node-sass）
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import './style/index.css';
+import './style/a.scss';
+
+function createDomElement(){
+    var dom = document.createElement('div');
+    dom.innerHTML = _.join(['aicoder.com','好！','线下视频'],'');
+    dom.className = 'box';
+    // dom.classList.add['box'];
+    return dom;
+}
+let divDom = createDomElement();
+
+document.body.appendChild(divDom);
